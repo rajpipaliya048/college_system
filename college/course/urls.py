452459fe4d4203365da45ab15fe0e3d246524d77
rajpipaliya048 <rajpipaliya2912@gmail.com>
2023-10-30 18:16:56 +0530

    feat: added stripw payment gateway

--- college/course/urls.py
+++ college/course/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import CreateCourseView, CourseListView, CourseEnrollView, CourseUnenrollView
+from .views import CreateCourseView, CourseListView, CourseEnrollView, CourseUnenrollView, StripePaymentView
 from . import views
 import re
 
@@ -10,8 +10,9 @@ urlpatterns = [
     path('list/', CourseListView.as_view(), name='course_list'),
     path('enroll/', CourseEnrollView.as_view(), name='course_enroll'),
     path('unenroll/', CourseUnenrollView.as_view(), name='course_unenroll'),
-    path('payment-done/', views.payment_done, name='payment_done'),
-    path('payment-cancelled/', views.payment_canceled, name='payment_cancelled'),
+    # path('payment-done/', views.payment_done, name='payment_done'),
+    # path('payment-cancelled/', views.payment_canceled, name='payment_cancelled'),
+    path('stripe_payment/', StripePaymentView.as_view(), name='stripe_payment'),
     re_path(r'^(?P<course_id>[\w-]+)/$', views.course_detail_view, name='detail'),
 
 ]
