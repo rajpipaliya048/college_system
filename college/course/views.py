1a7a65be6f105c7d7222f5f28910e7c795196ec3
rajpipaliya048 <rajpipaliya2912@gmail.com>
2023-11-08 12:33:01 +0530

    feat: added cashfree payment gateway

--- college/course/views.py
+++ college/course/views.py
@@ -3,6 +3,10 @@ import json
 import razorpay
 import requests
 import stripe
+import hmac
+import hashlib
+import time
+import random
 from .forms import CourseForm
 from .models import Course
 from .models import Course, Enrollment
@@ -53,22 +57,9 @@ class CourseEnrollView(View):
         course = get_object_or_404(Course, course_id=course_id)
         enrolled = Enrollment.objects.get_or_create(user_id=student, course_id=course, enrollment_date=current_date,)
         fees = course.fees * 100
-        
+        order_id = generate_unique_order_id()
         if course.fees != 0:    
-            return render(request, 'course/payment_form.html', {'course_id': course.course_id, 'amount': fees}) 
-            # host = request.get_host()
-
-            # paypal_dict = {
-            #     "business": settings.PAYPAL_RECEIVER_EMAIL,
-            #     "amount": course.fees,
-            #     "item_name": "name of the item",
-            #     "currency_code": "USD",
-            #     'notify_url': 'http://{}{}'.format(host,reverse('paypal-ipn')),
-            #     'return_url': 'http://{}{}'.format(host,reverse('course:payment_done')),
-            #     'cancel_return': 'http://{}{}'.format(host,reverse('course:payment_cancelled')),
-            # }
-            # form = PayPalPaymentsForm(initial=paypal_dict)
-            # return render(request, 'course/process_payment.html', {'course': course, 'form': form})
+            return render(request, 'course/payment_form.html', {'course_id': course.course_id, 'amount': fees, 'order_id': order_id}) 
             
         else:
             obj = get_object_or_404(Enrollment, user_id=student, course_id=course_id)
@@ -84,6 +75,24 @@ def payment_done(request):
 def payment_canceled(request):
     return render(request, 'course/payment_cancelled.html')
 
+class PaypalPaymentView(View):
+    def post(self, request):
+        course_id = self.request.POST.get('course_id')
+        course = get_object_or_404(Course, course_id=course_id)
+        host = request.get_host()
+
+        paypal_dict = {
+            "business": settings.PAYPAL_RECEIVER_EMAIL,
+            "amount": course.fees,
+            "item_name": "name of the item",
+            "currency_code": "USD",
+            'notify_url': 'http://{}{}'.format(host,reverse('paypal-ipn')),
+            'return_url': 'http://{}{}'.format(host,reverse('course:payment_done')),
+            'cancel_return': 'http://{}{}'.format(host,reverse('course:payment_cancelled')),
+        }
+        form = PayPalPaymentsForm(initial=paypal_dict)
+        return render(request, 'course/process_payment.html', {'course': course, 'form': form})
+    
 class StripePaymentView(View):
     def post(self, request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -110,7 +119,7 @@ class StripePaymentView(View):
         return redirect('dashboard')
  
 
-def initiate_payment(request):
+def initiate_razorpay_payment(request):
     if request.method == "POST":
         amount = int(request.POST["amount"]) * 100
 
@@ -140,6 +149,49 @@ def initiate_payment(request):
     
     return render(request, "payment.html")
 
+def initiate_cashfree_payment(request):
+    course_id = request.POST.get('course_id')
+    course = get_object_or_404(Course, course_id=course_id)
+    total_amount = course.fees
+    order_id = request.POST.get('order_id')
+    merchant_id = settings.CASHFREE_CLIENT_ID
+    secret_key = settings.CASHFREE_CLIENT_SECRET
+    payment_url = 'https://sandbox.cashfree.com/pg/orders'
+
+    payload = {
+        "customer_details": {
+            "customer_id": request.user.username,
+            'customer_name': request.user.first_name,
+            'customer_email': request.user.email,
+            'customer_phone': str(request.user.student.mobile_number),
+        },
+        "order_meta": {
+            "return_url": 'http://www.mycollege.com:8000/course/success/',
+            "notify_url": 'http://www.mycollege.com:8000/course/notify/', 
+        },
+        'order_id': order_id,  
+        'order_amount': str(total_amount),
+        'order_currency': 'INR',
+        'order_note': 'Payment for Course Enrollment',
+    }
+    headers = {
+    "accept": "application/json",
+    "x-api-version": "2022-09-01",
+    "content-type": "application/json",
+    "x-client-id": merchant_id,
+    "x-client-secret": secret_key
+}
+    response = requests.post(payment_url, json=payload, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        payment_session_id = data['payment_session_id']
+        order_id = data['order_id']
+        # import pdb; pdb.set_trace()
+        return render(request, 'course/cashfree_payment.html', {'payment_session_id': payment_session_id, 'order_id': order_id })
+        # return redirect("https://sandbox.cashfree.com/pg/orders/%s"%order_id, {'data': data})
+    else:
+        return JsonResponse({'error': 'Failed to initiate payment'})
+
 class CourseUnenrollView(View):
     def post(self, *args, **kwargs):
         course_id = self.request.POST.get('course_id')
@@ -155,4 +207,11 @@ def course_detail_view(request, course_id):
     return render(request, 'course/course_detail.html', {'course': course})
 
 
-        
+def generate_unique_order_id():
+    timestamp = int(time.time() * 1000)
+
+    random_number = random.randint(1000, 9999)
+
+    order_id = f"{timestamp}{random_number}"
+
+    return order_id
